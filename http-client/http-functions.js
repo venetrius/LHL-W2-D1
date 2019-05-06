f3256f63fc19748f019b187591161fab10f1a2cf
venetrius <gergelygjuhasz@gmail.com>
2019-05-06 18:56:26 +0000

    Step 5 - Make it a Module

--- http-client/http-functions.js
+++ http-client/http-functions.js
@@ -14,17 +14,3 @@ module.exports = function getHTML (options, callback) {
 
   });
 }
-
-/*
-function printHTML (html) {
-  console.log(html);
-}
-
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step4.html'
-};
-
-getHTML(requestOptions, printHTML);
-*/
